cec7e296039c2423359ac29fa6695c78941ab17c
Rukky <abrukevwe@gmail.com>
2023-05-31 07:12:21 +0100

    dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,16 @@ window.addEventListener("load", function () {
     var preloader = document.getElementById("preloader");
     preloader.style.display = "none";
 });
+
+
+// let darkmode = document.querySelector('#darkmode');
+
+// darkmode.onclick = () => {
+//     if (darkmode.classList.contains('bx-moon')){
+//        darkmode.classList.replace('bx-moon' , 'bx-sun');
+//        document.body.classList.add('color');
+//     }else{
+//         darkmode.classList.replace('bx-sun' , 'bx-moon');
+//         document.body.classList.remove('color');
+//     }
+// }
