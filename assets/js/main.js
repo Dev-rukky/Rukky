e62949e0c4aa5eb7809f24096b578b3a5c5e8530
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2023-06-20 15:48:17 +0100

    Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,6 +111,46 @@ function showButton3() {
   button4.style.display = "flex";
 }
 
+const track = document.querySelector('.slider-track');
+const slides = document.querySelectorAll('.slider-slide');
+const dots = document.querySelectorAll('.slider-dot');
+let i;
+
+const reset = () => dots.forEach((dot) => dot.classList.remove('active'));
+
+function slideTo(n) {
+  track.style.transform = `translateX(-${n * slides[0].offsetWidth}px)`;
+  reset();
+  dots[n].classList.add('active');
+}
+
+function activateArrows(direction) {
+  direction === 'right' ? i++ : i--;
+  if (i < 0) i = 0;
+  if (i > slides.length - 1) i = slides.length - 1;
+  slideTo(i);
+}
+
+function activateDots(e) {
+  i = e.target.dataset.index;
+  slideTo(i);
+}
+
+function activate(e) {
+  e.target.matches('.slider-dot') && activateDots(e);
+  e.target.matches('.arrow-right') && activateArrows('right');
+  e.target.matches('.arrow-left') && activateArrows();
+}
+
+function init(n) {
+  i = n;
+  slideTo(n);
+}
+
+document.addEventListener('click', activate, false);
+window.addEventListener('load', init(1), false);
+
+
 
 // let darkmode = document.querySelector('#darkmode');
 
